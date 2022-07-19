aa12df4f69dde46712cb6f251be68470e0c82b11
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-19 16:00:33 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -12,12 +12,7 @@ function trueOrFalse(val) {
 // 2) #7: if..else and ternary operator
 // http://www.codewars.com/kata/57202aefe8d6c514300001fd
 
-function saleHotdogs(n){
-  if (n < 5) {
-    return n * 100;
-  } else if (n >= 5 && n < 10) {
-    return n * 95;
-  } else if (n >= 10) {
-    return n * 90;
-  }
+function saleHotdogs(n) {
+  let multiply = n < 5 ? 100 : n >= 5 && n < 10 ? 95 : 90;
+  return n * multiply;
 }
