932a46d23b5ad40637a9b8270acb928a966d2509
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-19 16:42:45 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -13,3 +13,46 @@ function trueOrFalse(val) {
 // http://www.codewars.com/kata/57202aefe8d6c514300001fd
 
 const saleHotdogs = n => n * (n < 5 ? 100 : n < 10 ? 95 : 90);
+
+// 3) #8: Conditional statement--switch 
+// http://www.codewars.com/kata/572059afc2f4612825000d8a
+
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 1:
+      days = 31;
+      break;
+
+    case 2:
+      days = 28;
+      break;
+
+    case 3:
+      days = 31;
+      break;
+
+    case 5:
+      days = 31;
+      break;
+    case 7:
+      days = 31;
+      break;
+
+    case 8:
+      days = 31;
+      break;
+
+    case 10:
+      days = 31;
+      break;
+
+    case 12:
+      days = 31;
+      break;
+
+    default:
+      days = 30;
+  }
+  return days;
+}
