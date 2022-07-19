d283fb2e39a4028c8e9853cc853640dd84a0e258
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-19 14:33:31 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,1 +1,11 @@
+// 1) #6: Basic data types--Boolean and conditional statements if..else
+// https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+
+function trueOrFalse(val) {
+  if (val == false || val == null || val == undefined) {
+    return 'false';
+  } else {
+    return 'true';
+  }
+}
 
