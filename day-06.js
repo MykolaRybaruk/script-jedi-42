f3317e98ace6b5b296cf0152dc8c72b751660700
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-20 19:48:18 +0200

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,1 +1,14 @@
+// 1) Maximum Multiple
+// https://www.codewars.com/kata/maximum-multiple/train/javascript
 
+function maxMultiple(divisor, bound){
+    const arr = [];
+  for (let i = 0; i <= bound; i++) {
+    if (i % divisor === 0) {
+      arr.push(i);
+    }
+  }
+
+  const max = Math.max(...arr);
+  return max;
+}
