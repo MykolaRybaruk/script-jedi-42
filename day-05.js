a03f652c0ca2c1eb68e7ecddc1556ad91694ec74
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-20 17:27:45 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -3,3 +3,7 @@
 
 const correctTail = (body, tail) => body[body.length - 1] === tail[0] ? true : false ;
 
+// 2) Do I get a bonus
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+const bonusTime = (salary, bonus) => bonus == true ? '\u00A3' + salary * 10 : '\u00A3' + salary;
