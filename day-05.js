841417f8bd1d5e7d75b16fb54046c0c9e7ef3c65
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-20 16:59:34 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,13 +1,5 @@
 // 1) Syntax task
 // https://www.codewars.com/kata/is-this-my-tail/train/javascript
 
-function correctTail(body, tail) {
-  const bodyChar = body[body.length - 1];
-  const tailChar = tail[0];
-  if (bodyChar === tailChar) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const correctTail = (body, tail) => body[body.length - 1] === tail[0] ? true : false ;
 
