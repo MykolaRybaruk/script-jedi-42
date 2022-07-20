b123da5e946e190be60ae2045c58c7b2ab4f23ee
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-20 19:15:33 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -7,3 +7,19 @@ const correctTail = (body, tail) => body[body.length - 1] === tail[0] ? true : f
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96
 
 const bonusTime = (salary, bonus) => bonus == true ? '\u00A3' + salary * 10 : '\u00A3' + salary;
+
+
+// Automorphic number
+// https://www.codewars.com/kata/5a58d889880385c2f40000aa
+
+function automorphic(n){
+  const square = String(Math.pow(n, 2));
+  const nLength = String(n).length;
+  const last = square.slice(-nLength);
+
+  if (n == last) {
+    return 'Automorphic';
+  } else {
+    return 'Not!!';
+  }
+}
