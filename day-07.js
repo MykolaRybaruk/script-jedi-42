a26e5bb63712a46c03d0e71b2a52a598db2a3cc4
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-21 08:51:42 +0200

    Update day-07.js

--- day-07.js
+++ day-07.js
@@ -2,21 +2,17 @@
 // https://www.codewars.com/kata/55e7650c8d894146be000095/
 
 function validateMessage(msg) {
-
   if (msg == null) {
     throw new ReferenceError('Message is null!');
   }
 
   if (typeof msg !== 'string') {
-    throw new TypeError(
-      `Message should be of type string but was of type ${typeof msg}!`
-    );
+    throw new TypeError(`Message should be of type string but was of type ${typeof msg}!`);
   }
 
   if (msg.length === 0 || msg.length > 255) {
     throw new RangeError(`Message contains ${msg.length} characters!`);
   }
 
-  return (msg.includes('<') && msg.includes('>')) ? false : true;
-
+  return msg.includes('<') && msg.includes('>') ? false : true;
 }
