a8cce305ed63a05ca9b946a2df7af41b811a0f7a
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-22 14:57:39 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,1 +1,18 @@
+// 1) #16: Methods of String object--slice(), substring() and substr()
+// https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+function cutIt(arr) {
+  const length = arr.map((item) => {
+    return item.length; //returns items length
+  });
+  const minLength = Math.min(...length); //returns minimal length that will be used for slicing
+  const result = arr.map((item) => {
+    if (item.length === minLength) {
+      return item;
+    } else {
+      return item.slice(0, minLength);
+    }
+  });
+  return result;
+}
 
