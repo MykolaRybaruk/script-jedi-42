9a58bad1220af0a2772b0660c3e95815e9b80e18
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-23 09:51:33 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -1,1 +1,11 @@
+// 1) #19: Methods of String object--toUpperCase() toLowerCase() and replace()
+// http://www.codewars.com/kata/5728203b7fc662a4c4000ef3
 
+function alienLanguage(str) {
+  const upper = str.toUpperCase();
+  const words = upper.split(' ');
+  const result = words.map((item) => {
+    return item.slice(0, -1) + item[item.length - 1].toLowerCase();
+  });
+  return result.join(' ');
+}
