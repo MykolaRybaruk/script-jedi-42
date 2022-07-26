f2b08fdf9349cd26b49d1bc0d1a79675ebfd9903
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-26 21:43:20 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -16,3 +16,39 @@ function notEqual(s1, s2){
   return !areEqual(s1, s2);
 }
 
+
+
+
+// 2) Operations with Sets
+// https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+
+function process2Arrays(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  // finding elements present in both arrays
+  const intersection = new Set([...set1].filter((x) => set2.has(x)));
+
+  // finding unique arrays element
+  const uniqueInFirstArray = new Set([...set1].filter((x) => !set2.has(x)));
+  const uniqueInSecondArray = new Set(
+    [...set2].filter((item) => !set1.has(item))
+  );
+
+  const uniqueElementsInBothArrays =
+    uniqueInFirstArray.size + uniqueInSecondArray.size;
+
+  //finding elements that left after removing second array
+  const leftInArr1 = set1.size - intersection.size;
+
+  //finding elements that left after removing second array
+  const leftInArr2 = set2.size - intersection.size;
+  const result = [];
+  result.push(
+    intersection.size,
+    uniqueElementsInBothArrays,
+    leftInArr1,
+    leftInArr2
+  );
+  return result;
+}
