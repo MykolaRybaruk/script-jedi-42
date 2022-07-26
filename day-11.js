6d5c48f5fc03f9a41892e8e029f86bbe33657224
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-26 17:01:05 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -1,1 +1,26 @@
+//
+//
+
+function areEqual(s1, s2){
+  let set1 = [...s1];
+  let set2 = [...s2];
+  
+  if (set1.every(item => s2.has(item)) && set1.length == set2.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function notEqual(s1, s2){
+  let set1 = [...s1];
+  let set2 = [...s2];
+  
+  if (set1.every(item => s2.has(item)) && set1.length == set2.length) {
+    return false;
+  } else {
+    return true;
+  }
+
+}
 
