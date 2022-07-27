09e424e7aaea3f9272f6240eb42fe95413206d04
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-27 16:42:01 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -8,3 +8,25 @@ function wordToBin(str) {
   const result = binary.map((x) => '0' + x);
   return result;
 }
+
+
+// 2) Char Codes Calculation
+// https://www.codewars.com/kata/57f75cc397d62fc93d000059
+
+function calc(x) {
+  const letters = x.split('');
+  const codes = letters.map((x) => x.charCodeAt()).join('');
+  const stringNumbers = codes.split('');
+  const numbers = stringNumbers.map((x) => Number(x));
+  const total1 = numbers.reduce((prev, x) => prev + x, 0);
+  const replaced = numbers.map((x) => {
+    if (x === 7) {
+      return x - 6;
+    } else {
+      return x;
+    }
+  });
+  const total2 = replaced.reduce((prev, x) => prev + x, 0);
+
+  return total1 - total2;
+}
