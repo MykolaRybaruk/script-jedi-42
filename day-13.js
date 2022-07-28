39f0f1fe5ccf359c994519316c7d24f204638190
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-28 19:19:03 +0200

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -16,3 +16,41 @@ function match(candidate, job) {
 
   return false;
 }
+
+
+
+
+
+// 2) Job Matching #2
+// https://www.codewars.com/kata/56c2578be8b139bd5c001bd8
+
+function filterByEquity(candidates, job) {
+  const applicable = candidates.filter((x) => {
+    if (
+      (x.desiresEquity == true && job.equityMax > 0) ||
+      x.desiresEquity == false
+    ) {
+      return x;
+    }
+  });
+  return applicable;
+}
+
+function filterByLocation(candidates, job) {
+  const filtered = candidates.filter((x) => {
+    const candCities = new Set(x.desiredLocations);
+    candCities.add(x.currentLocation);
+    const jobCities = new Set(job.locations);
+    const inter = new Set([...candCities].filter((y) => jobCities.has(y)));
+    if (inter.size != 0) {
+      return x;
+    }
+  });
+  return filtered;
+}
+
+function match(job, candidates) {
+  const matchByEquity = filterByEquity(candidates, job);
+  const result = filterByLocation(matchByEquity, job);
+  return result;
+}
