deabc495402d9bcd8e677eb5d508261933771376
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-30 12:52:50 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -2,8 +2,8 @@
 // https://www.codewars.com/kata/59859f435f5d18ede7000050
 
 function wordToBin(str) {
-  const arr = str.split('');
-  const code = arr.map((x) => x.codePointAt(0));
+  const char = str.split('');
+  const code = char.map((x) => x.codePointAt(0));
   const binary = code.map((x) => x.toString(2));
   const result = binary.map((x) => '0' + x);
   return result;
