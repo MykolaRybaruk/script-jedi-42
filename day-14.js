083dd9f8c5272be02ca47f18a03d1489cf0216aa
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-07-30 15:06:57 +0200

    Update day-14.js

--- day-14.js
+++ day-14.js
@@ -1,1 +1,35 @@
+//[JEDI LEVEL] Job Matching #3
+// https://www.codewars.com/kata/56c2a067585d9ac8280003c9/
 
+function basicMatch(candidate, job) {
+  // checks if the candidate years can be boosted
+  if (candidate.preference === 'desired') {
+    candidate.experience *= 1.5;
+  }
+  // checks if the candidate's experiense is greater than job ideal years
+  if (candidate.experience < job.idealYears) {
+    return false;
+  }
+  
+  //compares skills names
+  if (candidate.name === job.name) {
+    return true;
+  }
+  return job.substitutions.some((skill) => skill.name === candidate.name);
+}
+
+function advancedMatch(candidate, job) {
+  // checks if the canidate would like to avoid the skills
+  if (
+    candidate.skills.some(
+      (skill) => skill.name === job.name && skill.preference === 'avoid'
+    )
+  ) {
+    return false;
+  }
+  return candidate.skills.some((candidate) => basicMatch(candidate, job));
+}
+
+function match(candidate, job) {
+  return job.skills.every((job) => advancedMatch(candidate, job));
+}
