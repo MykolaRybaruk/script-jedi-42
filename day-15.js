e7a58e9e36aeaa0e43c245129a4bd71ea02897d3
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-01 13:25:49 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -1,1 +1,28 @@
+//
+//
 
+
+
+
+
+
+
+
+
+// 2) loopArray
+// https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/
+
+function loopArr(arr, direction, steps) {
+  if (direction === 'left') {
+    for (let i = 0; i < steps; i++) {
+      let j = arr.shift();
+      arr.push(j);
+    }
+  } else if (direction === 'right') {
+    for (let i = 0; i < steps; i++) {
+      let item = arr.pop();
+      arr.unshift(item);
+    }
+  }
+  return arr;
+}
