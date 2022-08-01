e42c3e1dc58eab7485e60161dd5cd95f6b2bd9c4
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-01 15:18:21 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -26,3 +26,39 @@ function loopArr(arr, direction, steps) {
   }
   return arr;
 }
+
+
+
+
+// 3) #23: methods of arrayObject---push(), pop(), shift() and unshift()
+// http://www.codewars.com/kata/572af273a3af3836660014a1
+
+function infiniteLoop(arr, d, n) {
+  const allItems = arr[0].concat(arr[1].concat(arr[2]));
+  if (d === 'left') {
+    for (let i = 0; i < n; i++) {
+      const item = allItems.shift();
+      allItems.push(item);
+    }
+  }
+
+  if (d === 'right') {
+    for (let i = 0; i < n; i++) {
+      const item = allItems.pop();
+      allItems.unshift(item);
+    }
+  }
+
+  const firstPart = allItems.slice(0, arr[0].length);
+
+  const secondPart = allItems.slice(
+    arr[0].length,
+    arr[0].length + arr[1].length
+  );
+
+  const thirdPart = allItems.slice(arr[0].length + arr[1].length);
+
+  const result = [];
+  result.push(firstPart, secondPart, thirdPart);
+  return result;
+}
