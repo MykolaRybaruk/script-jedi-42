3ca79b5717be8b1cc1008812c020ddf2ba51bb94
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-02 09:44:17 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -1,1 +1,19 @@
+// #24: methods of arrayObject---splice() and slice()
+// http://www.codewars.com/kata/572cb264362806af46000793
+
+function threeInOne(arr) {
+  const arrCopy = [...arr];
+  const cuttedArray = [];
+  for (let i = 0; i < arr.length / 3; i++) {
+    const j = arrCopy.splice(0, 3);
+    cuttedArray.push(j);
+  }
+  const secondItemReversed = cuttedArray.map((x) =>
+    cuttedArray.indexOf(x) === 1 ? x.reverse() : x
+  );
+  const result = secondItemReversed.map((x) => {
+    return x.reduce((prev, item) => prev + item, 0);
+  });
+  return result;
+}
 
