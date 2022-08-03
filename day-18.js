c71f037e8ef8d30f55650ef9c3425a34d67e7a9d
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-03 15:18:45 +0200

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -33,6 +33,25 @@ function tailAndHead(arr) {
   });
 
   const result = multipliers.reduce((a, b) => a * b);
+  
+  
+  
+  
+  // #31: methods of arrayObject---isArray() indexOf() and toString()
+  // http://www.codewars.com/kata/5732b0351eb838d03300101d
+  
+  function blackAndWhite(arr) {
+  if (!Array.isArray(arr)) {
+    return `It's a fake array`;
+  }
+
+  if (arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1) {
+    return `It's a black array`;
+  } else {
+    return `It's a white array`;
+  }
+}
+
   return result;
 }
 
