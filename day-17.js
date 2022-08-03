632a7cbd37a6ed07a229108835f6b72abc3151e5
MykolaRybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-03 10:11:16 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -37,3 +37,16 @@ function mirrorImage(arr) {
 }
 
 
+
+
+
+
+// #29: methods of arrayObject---concat() and join()
+// http://www.codewars.com/kata/5731861d05d14d6f50000626
+
+function bigToSmall(arr) {
+  const flatArray = [].concat(...arr);
+  flatArray.sort((a, b) => b - a);
+  return flatArray.join('>');
+}
+
