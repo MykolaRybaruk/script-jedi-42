7dc39caeed08f8bc78896fcd05ae3122a6665629
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-04 15:28:38 +0200

    Update day-19.js

--- day-19.js
+++ day-19.js
@@ -9,3 +9,23 @@ function countAnimals(animals, count) {
   }
   return result;
 }
+
+
+
+
+
+// #38: Regular Expression--""^"",""$"", ""."" and test()
+// http://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+function findSimilarity(str,word){
+  const srtWordsArray = str.split(' ');
+
+  const wordChars = word[0] + word.slice(1, -1).replace(/./g, '.') + word[word.length - 1];
+
+  const filtered = srtWordsArray.filter((x) => {
+    if (x[0] + x.slice(1, -1).replace(/./g, '.') + x[x.length - 1] === wordChars) {
+      return x;
+    }
+  })
+  return filtered.join(' ');
+}
