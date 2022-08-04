251922c87e2b607f7819a686cbae63312cf6ca2d
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-04 13:09:40 +0200

    Update day-19.js

--- day-19.js
+++ day-19.js
@@ -1,1 +1,11 @@
+// #37: Unlock new weapon---RegExp Object
+// http://www.codewars.com/kata/5735e39313c205fe39001173
 
+function countAnimals(animals, count) {
+  const result = [];
+  for (const an of count) {
+    const a = (animals.match(new RegExp(an, 'g')) || []).length;
+    result.push(a);
+  }
+  return result;
+}
