d9e13a8ac76143db9ea5a800744ceee7514e45ad
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-05 14:37:31 +0200

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -2,3 +2,9 @@
 // https://www.codewars.com/kata/573bca07dffc1aa693000139
 
 const regex = /^-?9+.*0{4,}$/;
+
+
+// #40: Regular Expression--""|"", ""[]"" and ""()""
+// http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex=/(http:\/\/|https:\/\/)[a-z0-9\.]+(\.com|\.net)/ig;
