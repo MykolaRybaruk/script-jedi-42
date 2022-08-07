cd7b3897b91d1388b2aef77798decdcabd35a071
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-07 15:46:06 +0200

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -2,3 +2,14 @@
 // http://www.codewars.com/kata/573e6831e3201f6a9b000971 
 
 const regex=/\b(\w)(\w?)(\w?).?\3\2\1\b/g;
+
+
+
+
+// [JEDI LEVEL]  #42: Regular Expression--(?:), (?=) and (?!)
+// http://www.codewars.com/kata/573fb9223f9793e485000453
+
+const regex=/(\d)(?=(\d{3})+$)/g;
+function addCommas(money,reg){
+  return money.replace(reg,x=>x+",")  //like this
+}
