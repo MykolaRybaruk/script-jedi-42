0caa4fe38e4d04266c38d4a9c8291f3f5ccc931b
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-08 14:57:13 +0200

    Update day-22.js

--- day-22.js
+++ day-22.js
@@ -4,3 +4,8 @@
 function anyArrows(arrows){
   return arrows.some((x) => x.damaged !== true);
 }
+
+// Currying functions: multiply all elements in an array
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+
+const multiplyAll = arr => mult => arr.map((x) => x * mult);
