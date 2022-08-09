8ab07e4079ba32f92e8f629222afede8fe35c26b
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-09 10:32:43 +0200

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -1,1 +1,8 @@
+// Duplicate arguments
+// http://www.codewars.com/kata/duplicate-arguments
 
+function solution(...args){
+  const set1 = new Set([...args]);
+  const argsCopy = [...args];
+  return set1.size !== argsCopy.length;
+}
