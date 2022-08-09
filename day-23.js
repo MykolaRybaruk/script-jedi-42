aed5bf18c178bfdd043001bd1bd1b567f0e7dfaf
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-09 10:07:08 +0200

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -4,3 +4,13 @@
 function spread(func, args) {
   return func(...args);
 }
+
+
+
+
+// For the sake of argument
+// http://www.codewars.com/kata/for-the-sake-of-argument
+
+const numbers = (...args) => {
+  return [...args].every((x) => typeof x === 'number');  
+}
