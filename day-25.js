125eee3b046811ebc74615f5c6fca12a7c0ebeda
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-10 16:47:29 +0200

    Update day-25.js

--- day-25.js
+++ day-25.js
@@ -12,3 +12,10 @@ const alpha = {
     }
 };
 
+
+
+
+// Take an Arrow to the knee, Functionally
+// https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+
+const ArrowFunc = (arr) => arr.map((x) => String.fromCharCode(x)).join('');
