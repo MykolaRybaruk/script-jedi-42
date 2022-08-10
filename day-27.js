4c812ccb7289f6575a4f441ce1c506fd9aae4da2
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-10 22:14:09 +0200

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -1,1 +1,12 @@
+// Understanding closures - the basics
+// https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
 
+function buildFun(n){
+	const res = [];
+	for (let i = 0; i < n; i++){
+		res.push(function(){
+			return i;
+		})
+	}
+	return res
+}
