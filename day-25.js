9dca5f544508d3fd0f3c90157167db7829935eda
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-10 14:48:47 +0200

    Update day-25.js

--- day-25.js
+++ day-25.js
@@ -1,1 +1,14 @@
+//
+//
+
+const name = 'The Window';
+
+const alpha = {
+    name : 'My Alpha',
+    getNameFunc : function() {
+        return function() {
+            return this.name;
+        }.bind(this);
+    }
+};
 
