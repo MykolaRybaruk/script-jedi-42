e0bf49ba3175bfc0d8444cbcf23a048ddb8ad86f
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-10 17:17:31 +0200

    Update day-26.js

--- day-26.js
+++ day-26.js
@@ -1,1 +1,4 @@
+// Sort with Arrow Functions
+// https://www.codewars.com/kata/sort-with-arrow-functions
 
+const OrderPeople = (people) => people.sort((a, b) => a.age - b.age); //підкажіть буь-ласка чому на кодварс назва константи написана з великої літери
