55c0e2c164a6f61b69087d8041d7cb8a0ca15caf
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-11 13:43:25 +0200

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -1,1 +1,8 @@
+// Function within a function (Submit може не працювати, перевірте чи проходить Attempt)
+// http://www.codewars.com/kata/a-function-within-a-function
 
+function always (n) {
+  return function () {
+    return n;
+  }
+}
