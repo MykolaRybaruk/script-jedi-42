c7cb3a772d9801b7ceac85ba834962805bd06b47
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-16 12:07:40 +0200

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -4,3 +4,15 @@
 const myObj = {__proto__ : MyObject.prototype};
 
 MyObject.call(myObj);
+
+
+
+
+// Replicate new
+// http://www.codewars.com/kata/replicate-new
+
+function nouveau (Constructor, ...arguments) {
+  const instance = Object.create(Constructor.prototype);
+  const binded = Constructor.bind(instance)(...arguments);
+  return Object(binded) === binded ? binded : instance;
+}
