77ca433fe6bf8f3b955d7424c32a184cfc9dcb59
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-17 10:13:39 +0200

    Update day-31.js

--- day-31.js
+++ day-31.js
@@ -1,1 +1,8 @@
+// Singleton pattern 
+// https://www.codewars.com/kata/singleton-pattern/train/javascript
 
+const Singleton = function(){
+  const instance = Singleton.instance;
+  if (instance) return instance;
+  Singleton.instance = this;
+};
