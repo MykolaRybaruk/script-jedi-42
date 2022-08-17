b758d293c20db5c11cbbda778ef6970651619f51
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-17 13:24:52 +0200

    Update day-31.js

--- day-31.js
+++ day-31.js
@@ -6,3 +6,26 @@ const Singleton = function(){
   if (instance) return instance;
   Singleton.instance = this;
 };
+
+
+// [JEDI LEVEL] Simple Web Framework #1: Create a basic router
+// https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
+
+class Router {
+    
+    constructor() {
+        this.routes = new Map();
+    }  
+        
+    bind(url, method, action) {
+        this.routes.set(url + ":" + method, action);
+    }
+    
+    runRequest(url, method) {
+        if (!this.routes.has(url + ":" + method)) {
+            return "Error 404: Not Found";
+        }
+        return this.routes.get(url + ":" + method)();
+    }
+    
+}
