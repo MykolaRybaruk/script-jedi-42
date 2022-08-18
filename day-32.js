f469b8aa33a5e693e634264ac09e3f37ae049871
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-18 13:46:27 +0200

    Update day-32.js

--- day-32.js
+++ day-32.js
@@ -7,3 +7,31 @@ function NameMe(first, last) {
   this.name = this.firstName + ' ' + this.lastName; 
   return this.name;
 };
+
+
+
+
+
+// ""this"" is an other problem
+// https://www.codewars.com/kata/this-is-an-other-problem
+
+class NamedOne {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  set fullName(text) {
+    let arr = text.split(' ');
+    if (arr.length <= 1) {
+      return true;
+    } else {
+      this.firstName = arr[0];
+      this.lastName = arr[1];
+    }
+  }
+}
