013bfb0f5acee1c4d60beffde8625ecd643ad18f
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-20 16:37:03 +0200

    Update day-33.js

--- day-33.js
+++ day-33.js
@@ -2,15 +2,15 @@
 // https://www.codewars.com/kata/array-number-reduce/train/javascript
 
 Array.prototype.reduce = function(process, initial) {
-  var reduction;
+  let reduction;
   if(initial == undefined){
     reduction = this[0];
-    for(var i = 1; i < this.length; i++){
+    for(let i = 1; i < this.length; i++){
       reduction = process(reduction, this[i]);
     }
   } else {
     reduction = initial;
-    for(var i = 0; i < this.length; i++){
+    for(let i = 0; i < this.length; i++){
       reduction = process(reduction, this[i]);
      }
   }
