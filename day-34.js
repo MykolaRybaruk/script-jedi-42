5be991a23569635d504a5286e9f829303f4f96f7
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-20 19:35:57 +0200

    Update day-34.js

--- day-34.js
+++ day-34.js
@@ -1,1 +1,11 @@
+// Adam and Eve
+// https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
 
+class God{
+  static create(){
+    return [new Man, new Woman];
+  }
+}
+class Human{}
+class Man extends Human{}
+class Woman extends Human{}
