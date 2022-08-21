dbed0d281e9e31fc45c76b0d9f656c5ae93009d1
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-21 17:34:36 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -1,7 +1,7 @@
 // Object Prototypes
 // https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
 
-Object.prototype.description = 'TODO: GIVE ME A DESCRIPTION'; 
+widget.__proto__.description = 'TODO: GIVE ME A DESCRIPTION'
 
 
 
