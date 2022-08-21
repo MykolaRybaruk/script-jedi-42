a7b6d78ed1da1c8ffdaf6d5b15af8e6db5b37ee4
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-21 17:22:59 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -2,3 +2,19 @@
 // https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
 
 Object.prototype.description = 'TODO: GIVE ME A DESCRIPTION'; 
+
+
+
+
+
+// Class-Like Objects
+// https://www.codewars.com/kata/javascript-class-like-objects
+
+class Animal {
+  constructor (name, type) {
+    this.name = name;
+    this.type = type;
+    this.toString = function (name, type) {
+      return `${this.name} is a ${this.type}`;}
+  };
+} 
