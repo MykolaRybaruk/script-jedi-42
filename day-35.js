696a9bc3ff210c30a00552dea81fca0a24e9daf3
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-23 19:01:47 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -28,7 +28,7 @@ class Animal {
 Function.prototype.bind = function (ctx) {
   const func = this;
   return function (...args) {
-    const rightCtx = this === global ? ctx : this;
-    return func.apply(rightCtx, args);
+    const correctCtx = this === global ? ctx : this;
+    return func.apply(correctCtx, args);
   }
 };
