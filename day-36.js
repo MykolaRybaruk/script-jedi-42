63bf0089fef114132e1e4330854f571aab5961df
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-24 12:26:02 +0200

    Update day-36.js

--- day-36.js
+++ day-36.js
@@ -31,3 +31,44 @@ async function submitOrder(user) {
     orderSuccessful ? "was" : "was NOT"
   } placed successfully;
 }
+
+
+
+
+
+// This isn't what you think!
+// https://www.codewars.com/kata/5876a9f6b09010b1b4000191/train/javascript
+
+ShoppingCart.prototype.addButtonClicked = function(item) {
+  // Check if there's any of the item available
+  // The first technique is using Hard binding with the `bind` method.
+  this.checkQuantityAsync(item, this.addButtonClicked1.bind(this));
+};
+
+ShoppingCart.prototype.addButtonClicked1 = function({item, quantity}) {
+  // If the item was in stock, add one to our cart
+  if (quantity > 0) {
+    // The second technique is using an arrow function
+    // which allows us to use the `this` keyword as a "lexical this"
+    // according to @getify the arrow function doesn't have a `this`
+    // in his scope, so it performs a lookup for the closest upper `this` defined.
+    this.addToCartAsync(item, 1, (...params) => this.addButtonClicked2(...params));
+  }
+};
+
+ShoppingCart.prototype.addButtonClicked2 = function(success) {
+  // If it was added to the cart, then refresh the display
+  if (success) {
+    // The third technique is using the lexical scope with an explicit binding
+    // using the `apply` method.
+    var context = this;
+    this.updateCartDisplayAsync(function(){
+      context.addButtonClicked3.apply(context, arguments)
+    });
+  }
+};
+
+ShoppingCart.prototype.addButtonClicked3 = function(success) {
+  // Log the success or failure of our updates
+  this.showMessage(`${success? "Successfully" : "Unsuccessfully"} added item to cart`);
+};
