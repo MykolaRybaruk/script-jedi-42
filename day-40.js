ab4d1392a0aaa78c933f3cd7c99e56a3fc6b8804
Mykola Rybaruk <91557804+MykolaRybaruk@users.noreply.github.com>
2022-08-27 21:04:48 +0200

    Update day-40.js

--- day-40.js
+++ day-40.js
@@ -1,1 +1,11 @@
+// Node.js Async IO
+// https://www.codewars.com/kata/542106e2dda52658bf00001a/train/javascript
 
+const { exec } = require('child_process');
+
+async function execute(command, callback) {
+  exec(command, (err, data) => {
+    if (err) { callback(err) }
+    else { callback(null, data) }
+  });
+}
